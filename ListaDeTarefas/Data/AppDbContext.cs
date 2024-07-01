c406702ed708c4d34d637a5eba52e28257b7c294
Alexandre Direito <xandedsj@gmail.com>
2024-07-01 18:36:06 -0300

    Ligação com o banco de dados feita!

---
+++ ListaDeTarefas/Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using ListaDeTarefas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListaDeTarefas.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        } 
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>().HasData(
+                    new Categoria { CategoriaId = "trabalho", Nome = "Trabalho"},
+                    new Categoria { CategoriaId = "casa", Nome = "Casa" },
+                    new Categoria { CategoriaId = "faculdade", Nome = "Faculdade" },
+                    new Categoria { CategoriaId = "concurso", Nome = "Concurso" },
+                    new Categoria { CategoriaId = "compras", Nome = "Compras" }
+                );
+
+            modelBuilder.Entity<Status>().HasData(
+                    new Status { StatusId = "aberto", Nome = "Aberto"},
+                    new Status { StatusId = "completo", Nome = "Completo"}
+                );
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
