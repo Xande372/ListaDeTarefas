d67d99eea2ec23db23608b530e6aff1f40050ef5
Alexandre Direito <xandedsj@gmail.com>
2024-07-02 14:11:14 -0300

    funcionalidade filtrar: ok

--- ListaDeTarefas/Controllers/HomeController.cs
+++ ListaDeTarefas/Controllers/HomeController.cs
@@ -1,14 +1,73 @@
+using ListaDeTarefas.Data;
 using ListaDeTarefas.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace ListaDeTarefas.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+        public HomeController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+
+        public IActionResult Index(string id)
+        {
+            var filtros = new Filtros(id);
+
+            ViewBag.Filtros = filtros;
+            ViewBag.Categorias = _context.Categorias.ToList();
+            ViewBag.Status = _context.Statuses.ToList();
+            ViewBag.VencimentoValores = Filtros.VencimentoValoresFiltro;
+
+            IQueryable<Tarefa> consulta = _context.Tarefas
+                .Include(c => c.Categoria)
+                .Include(s => s.Status);
+
+            if (filtros.TemCategoria)
+            {
+                consulta = consulta.Where(t => t.CategoriaId == filtros.CategoriaId);
+            }
+
+            if (filtros.TemStatus)
+            {
+                consulta = consulta.Where(t => t.StatusId == filtros.StatusId);
+            }
+
+            if (filtros.TemVencimento)
+            {
+                var presente = DateTime.Today;
+
+                if (filtros.EhPassado)
+                {
+                    consulta = consulta.Where(t => t.DataVencimento < presente);
+                }
+
+                if (filtros.EhFuturo)
+                {
+                    consulta = consulta.Where(t => t.DataVencimento > presente);
+                }
+
+                if (filtros.EhPresente)
+                {
+                    consulta = consulta.Where(t => t.DataVencimento == presente);
+                }
+            }
+
+            var tarefas = consulta.OrderBy(t => t.DataVencimento).ToList();
+
+            return View(tarefas);
+        }
+
+        [HttpPost]
+        public IActionResult Filtrar(string[] filtro)
+        {
+            string id = string.Join("-", filtro);
+            return RedirectToAction("Index", new {ID = id});
         }
     }
 }
